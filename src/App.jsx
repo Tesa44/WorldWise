6c96627654f3e7da98cb0f1e245908488e98b30e
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-07-15 15:14:28 +0200

    Optimize bundle size with code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,73 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { lazy } from "react";
+import { Suspense } from "react";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import Login from "./pages/Login";
+// import AppLayout from "./pages/AppLayout";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage></Homepage>}></Route>
-            <Route path="product" element={<Product></Product>}></Route>
-            <Route path="pricing" element={<Pricing></Pricing>}></Route>
-            <Route path="login" element={<Login></Login>}></Route>
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout></AppLayout>
-                </ProtectedRoute>
-              }
-            >
-              {/* Here we have problem, because we see cities list, but URL is only .../app and doesnt include /cities
+          <Suspense fallback={<SpinnerFullPage></SpinnerFullPage>}>
+            <Routes>
+              <Route index element={<Homepage></Homepage>}></Route>
+              <Route path="product" element={<Product></Product>}></Route>
+              <Route path="pricing" element={<Pricing></Pricing>}></Route>
+              <Route path="login" element={<Login></Login>}></Route>
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout></AppLayout>
+                  </ProtectedRoute>
+                }
+              >
+                {/* Here we have problem, because we see cities list, but URL is only .../app and doesnt include /cities
            <Route
             index
             element={
               <CityList cities={cities} isLoading={isLoading}></CityList>
             }
           ></Route> */}
-              {/* To fix it we use Navigate component, which redirects to correct URL without clicking on any link - declarative way */}
-              <Route
-                index
-                element={<Navigate replace to="cities"></Navigate>}
-              ></Route>
-              <Route path="cities" element={<CityList></CityList>}></Route>
-              <Route path="cities/:id" element={<City></City>}></Route>
-              <Route
-                path="countries"
-                element={<CountryList></CountryList>}
-              ></Route>
-              <Route path="form" element={<Form></Form>}></Route>
-            </Route>
-            <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
-          </Routes>
+                {/* To fix it we use Navigate component, which redirects to correct URL without clicking on any link - declarative way */}
+                <Route
+                  index
+                  element={<Navigate replace to="cities"></Navigate>}
+                ></Route>
+                <Route path="cities" element={<CityList></CityList>}></Route>
+                <Route path="cities/:id" element={<City></City>}></Route>
+                <Route
+                  path="countries"
+                  element={<CountryList></CountryList>}
+                ></Route>
+                <Route path="form" element={<Form></Form>}></Route>
+              </Route>
+              <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
